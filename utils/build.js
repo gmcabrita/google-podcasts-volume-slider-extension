6f646dd913c9658e6e08cb41ce1a78824818c72a
Gonçalo Cabrita <_@gmcabrita.com>
2020-04-04 15:57:54 +0100

    Run prettier on Webpack files too

--- utils/build.js
+++ utils/build.js
@@ -1,9 +1,8 @@
 var webpack = require("webpack"),
-    config = require("../webpack.config");
+  config = require("../webpack.config");
 
 delete config.chromeExtensionBoilerplate;
 
-webpack(
-  config,
-  function (err) { if (err) throw err; }
-);
+webpack(config, function (err) {
+  if (err) throw err;
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,29 @@
 var webpack = require("webpack"),
-    path = require("path"),
-    fileSystem = require("fs"),
-    env = require("./utils/env"),
-    CleanWebpackPlugin = require("clean-webpack-plugin").CleanWebpackPlugin,
-    CopyWebpackPlugin = require("copy-webpack-plugin"),
-    HtmlWebpackPlugin = require("html-webpack-plugin"),
-    WriteFilePlugin = require("write-file-webpack-plugin");
+  path = require("path"),
+  fileSystem = require("fs"),
+  env = require("./utils/env"),
+  CleanWebpackPlugin = require("clean-webpack-plugin").CleanWebpackPlugin,
+  CopyWebpackPlugin = require("copy-webpack-plugin"),
+  HtmlWebpackPlugin = require("html-webpack-plugin"),
+  WriteFilePlugin = require("write-file-webpack-plugin");
 
 // load the secrets
 var alias = {};
 
-var secretsPath = path.join(__dirname, ("secrets." + env.NODE_ENV + ".js"));
+var secretsPath = path.join(__dirname, "secrets." + env.NODE_ENV + ".js");
 
-var fileExtensions = ["jpg", "jpeg", "png", "gif", "eot", "otf", "svg", "ttf", "woff", "woff2"];
+var fileExtensions = [
+  "jpg",
+  "jpeg",
+  "png",
+  "gif",
+  "eot",
+  "otf",
+  "svg",
+  "ttf",
+  "woff",
+  "woff2",
+];
 
 if (fileSystem.existsSync(secretsPath)) {
   alias["secrets"] = secretsPath;
@@ -24,78 +35,85 @@ var options = {
     podcasts: [
       path.join(__dirname, "src", "js", "podcasts.js"),
       path.join(__dirname, "src", "css", "podcasts.scss"),
-    ]
+    ],
   },
   chromeExtensionBoilerplate: {
-    notHotReload: ["podcasts"]
+    notHotReload: ["podcasts"],
   },
   output: {
     path: path.join(__dirname, "build"),
-    filename: "[name].bundle.js"
+    filename: "[name].bundle.js",
   },
   module: {
     rules: [
       {
         test: /\.css$/,
         loader: "style-loader!css-loader",
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.scss$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[name].bundle.css',
+              name: "[name].bundle.css",
             },
           },
-          { loader: 'extract-loader' },
-          { loader: 'css-loader' },
+          { loader: "extract-loader" },
+          { loader: "css-loader" },
           {
-            loader: 'sass-loader',
+            loader: "sass-loader",
             options: {
               // Prefer Dart Sass
-              implementation: require('sass'),
+              implementation: require("sass"),
               sassOptions: {
-                includePaths: ['./node_modules']
+                includePaths: ["./node_modules"],
               },
             },
           },
-        ]
+        ],
       },
       {
-        test: new RegExp('.(' + fileExtensions.join('|') + ')$'),
+        test: new RegExp(".(" + fileExtensions.join("|") + ")$"),
         loader: "file-loader?name=[name].[ext]",
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.html$/,
         loader: "html-loader",
-        exclude: /node_modules/
-      }
-    ]
+        exclude: /node_modules/,
+      },
+    ],
   },
   resolve: {
-    alias: alias
+    alias: alias,
   },
   plugins: [
     // clean the build folder
     new CleanWebpackPlugin(),
     // expose and write the allowed env vars on the compiled bundle
     new webpack.EnvironmentPlugin(["NODE_ENV"]),
-    new CopyWebpackPlugin([{
-      from: "src/manifest.json",
-      transform: function (content, path) {
-        // generates the manifest file using the package.json informations
-        return Buffer.from(JSON.stringify({
-          description: process.env.npm_package_description,
-          version: process.env.npm_package_version,
-          ...JSON.parse(content.toString())
-        }))
-      }
-    }], { copyUnmodified: true }),
-    new WriteFilePlugin()
-  ]
+    new CopyWebpackPlugin(
+      [
+        {
+          from: "src/manifest.json",
+          transform: function (content, path) {
+            // generates the manifest file using the package.json informations
+            return Buffer.from(
+              JSON.stringify({
+                description: process.env.npm_package_description,
+                version: process.env.npm_package_version,
+                ...JSON.parse(content.toString()),
+              })
+            );
+          },
+        },
+      ],
+      { copyUnmodified: true }
+    ),
+    new WriteFilePlugin(),
+  ],
 };
 
 if (env.NODE_ENV === "development") {
